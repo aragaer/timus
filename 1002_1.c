7436672796dcabe97dcec358cef1728db1903a61
Aragaer <aragaer@gmail.com>
2012-02-01 19:28:21 +0400

    Solved

--- 1002_1.c
+++ 1002_1.c
@@ -20,25 +20,29 @@ struct node_s {
 char num_from_letter[256], nl_code[] = "22233344115566070778889990";
 
 char number[101];
-char *solution[200];
-int known_solutions[101];
+struct solution {
+    int len;
+    char *word;
+    struct solution *next;
+} known_solutions[101];
 
 #define NO_BEST 101
 
 #ifndef ONLINE_JUDGE
 #define debug(...) printf(__VA_ARGS__)
 void print_tree(struct node_s *node, int depth) {
-    int i;
-    for (i = 0; i < depth; i++)
-        printf("\t");
-    printf("%s", node->name);
+    int i, j;
     if (node->word)
         printf(" (w)\n");
     else
         printf("\n");
     for (i = 0; i < 10; i++)
-        if (node->children[i])
+        if (node->children[i]) {
+            for (j = 0; j < depth; j++)
+                printf(" ");
+            printf("%d", i);
             print_tree(node->children[i], depth+1);
+        }
 }
 #else
 #define debug(...)
@@ -49,50 +53,60 @@ static __inline struct node_s *new_node(void) {
     int i;
     struct node_s *new = pool + pool_used;
     pool_used++;
+    new->word = NULL;
     for (i = 0; i < 10; i++)
         new->children[i] = NULL;
     return new;
 }
 
-int try_solve(char *num, int limit) {
+struct solution *solve_from_position(int pos) {
+    char *num = number + pos;
     struct node_s *ptr = &root;
+    struct solution *s = known_solutions + pos, *f;
 #ifndef ONLINE_JUDGE
-    printf("%s(%s, %d)\n", __func__, num, limit);
+    int i;
+    printf("%s(%d) = %s\n", __func__, pos, num);
 #endif
-    while (1) {
-        int n;
-        if (!*num)      // that's the end of the number
-            if (ptr->word) {    // we won
-                debug("Everyting ends with '%s'\n", ptr->word);
-                solution[0] = ptr->word;
-                return 1;
-            } else {
-                debug("number ended. that was wrong way... return %d\n", limit+1);
-                return limit;
-            }
+    if (s->len)
+        return s;
 
-        // number goes on
-        n = *num - '0';
-        if (ptr->word) { // fork here
-            int *res = known_solutions + (num - number);
+    s->len = NO_BEST;
+    for (; number[pos]; pos++) { // number goes on
+        int n = number[pos] - '0';
+        if (ptr->word) { // we can end the word here, let's see what happens
             debug("accept word '%s' and continue\n", ptr->word);
-            if (*res == 0)
-                *res = try_solve(num, limit - 1);
-            if (*res < limit - 1) {
-                solution[*res] = ptr->word;
-                limit = *res+1;
+            f = solve_from_position(pos);
+            if (f->len + 1 < s->len) {
+                debug("word '%s' is good, we've got us a %d words-long subsolution which is better than %d\n", ptr->word, f->len+1, s->len);
+                s->word = ptr->word;
+                s->len = f->len + 1;
+                s->next = f;
             }
         }
+#ifndef ONLINE_JUDGE
+        for (i = 0; i < 10; i++)
+            if (ptr->children[i])
+                printf("can pick code %d\n", i);
+#endif
 
-        // we have no choice - keep going
-        if (ptr->children[n]) { // alright!
+        // keep going on a longer word
+        if (ptr->children[n]) // alright!
             ptr = ptr->children[n];
-            num++;
-        } else {
-            debug("can't continue. that was wrong way. return %d\n", limit+1);
-            return limit; // can't advance
-        }
+        else // can't advance
+            return s;
+        debug("acceptable code %d in position %d\n", n, pos);
     }
+    debug("word ended\n");
+
+// that's the end of the number
+
+    if (ptr->word) {    // we won
+        debug("Everyting ends with '%s'\n", ptr->word);
+        s->len = 1;
+        s->word = ptr->word;
+        s->next = NULL;
+    }
+    return s;
 }
 
 int main() {
@@ -103,6 +117,7 @@ int main() {
     while (1) {
         int l;
         char letter, *word, *wptr;
+        struct solution *s;
         scanf("%s", number);
         if (number[0] == '-')
             break;
@@ -115,7 +130,7 @@ int main() {
         for (i = 0; i < 10; i++)
             root.children[i] = NULL;
         for (i = 0; i < 101; i++)
-            known_solutions[i] = 0;
+            known_solutions[i].len = 0;
         pool_used = 0;
 
         (void) scanf("%d", &dict_size);
@@ -132,41 +147,28 @@ int main() {
 
                 if (*wptr == '\n') {
                     *wptr = '\0';
-#ifndef ONLINE_JUDGE
-                    if (ptr == &root) {
-                        printf("the fuck?! word %d of %d %s\n", i, dict_size, word);
-                        exit(1);
-                    }
-#endif
-                    if (ptr->word == NULL) {
+                    if (ptr->word == NULL)
                         ptr->word = word;
-                    }
                     break;
                 }
 
                 n = num_from_letter[*wptr] - '0';
 
-                if (ptr->children[n] == NULL) {
-                    struct node_s *new = new_node();
-#ifndef ONLINE_JUDGE
-                    memcpy(new->name, word, wptr-word+1);
-                    new->name[wptr-word+1] = '\0';
-#endif
-                    ptr->children[n] = new;
-                }
+                if (ptr->children[n] == NULL)
+                    ptr->children[n] = new_node();
                 ptr = ptr->children[n];
             };
         }
 #ifndef ONLINE_JUDGE
-//        print_tree(&root, 0);
+        print_tree(&root, 0);
         printf("used up %d nodes\n", pool_used);
 #endif
-        i = try_solve(number, NO_BEST);
-        if (i >= NO_BEST)
+        solve_from_position(0);
+        if (known_solutions->len == NO_BEST)
             printf("No solution.\n");
         else
-            while (i--)
-                printf("%s%c", solution[i], i ? ' ' : '\n');
+            for (s = known_solutions; s; s = s->next)
+                printf("%s%c", s->word, s->next ? ' ' : '\n');
     }
 }
 
