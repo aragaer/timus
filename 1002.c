6e2a9b3f955ee885750924a896b5a6a5240624be
Aragaer <aragaer@gmail.com>
2012-02-01 15:00:52 +0400

    Should be even faster but still too slow

--- 1002.c
+++ 1002.c
@@ -48,19 +48,41 @@ int try_solve(char *num, int limit) {
     int res = 0;
     struct dict_s *d = dict + num[0] - '0';
     struct word *w;
+    int l = strlen(num);
+
+    int buf[MAX_WORD_LEN/sizeof(int) + 1];
+    memcpy(buf, num, l + 1);
+    num = (char *) buf;
 
 #ifndef ONLINE_JUDGE
     printf("%s(%s, %d)\n", __func__, num, limit);
     print_dict(num[0]);
 #endif
-
+//    printf("num is %s\n", num);
     for (w = d->head; w; w = w->next) {
-        while (w && strncmp(num, w->code, w->len))
-            w = w->next;
+        if (w->len > l)
+            continue;
+//        printf("word %s, code %s\n", w->word, w->code);
 
-        if (!w)
-            return limit + 1;
-        
+        // fast implementation of strncmp
+        int diffs = 0;
+        int i, *iw = (int *) w->code, *in = (int *) num;
+        for (i = 0; i < w->len/sizeof(int); i++) {
+//            printf("%d: %c%c%c%c vs %c%c%c%c\n", i, w->code[i*4],
+//                w->code[i*4+1], w->code[i*4+1], w->code[i*4+3],
+//                num[i*4], num[i*4+1], num[i*4+2], num[i*4+3]);
+//            printf("%d: %08X vs %08X\n", i, *iw, *in);
+            diffs |= *iw++ ^ *in++;
+        }
+
+        for (i *= sizeof(int); i < w->len; i++) {
+//            printf("%d: %02X vs %02X\n", i, w->code[i], num[i]);
+            diffs |= w->code[i] ^ num[i];
+        }
+
+        if (diffs)
+            continue;
+
 #ifndef ONLINE_JUDGE
         printf("word %s matches!\n", w->word);
 #endif
@@ -95,6 +117,7 @@ int main() {
     for (i = 'a'; i <= 'z'; i++)
         num_from_letter[i] = nl_code[i - 'a'];
     while (1) {
+        int l;
         scanf("%s", number);
         for (i = 0; i < 10; i++)
             dict[i].head = dict[i].tail = NULL;
@@ -104,12 +127,18 @@ int main() {
         if (number[0] == '-')
             break;
 
+        l = strlen(number);
+
         scanf("%d", &dict_size);
         for (i = 0; i < dict_size; i++) {
             struct dict_s *d;
             struct word *w;
+            int wl;
 
             scanf("%s", tmp);
+            wl = strlen(tmp);
+            if (wl > l) // The word is too long - ignore it
+                continue;
             d = dict + num_from_letter[tmp[0]] - '0';
             w = words + i;
             if (d->tail)
@@ -117,7 +146,7 @@ int main() {
             else
                 d->head = w;
             d->tail = w;
-            w->len = strlen(tmp);
+            w->len = wl;
             memcpy(w->word, tmp, w->len + 1);
             encode(w);
 #ifndef ONLINE_JUDGE
